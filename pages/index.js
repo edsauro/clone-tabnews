42deb4692dfc9ac718cf5437e949b01b10fe7e3c
saorim <edusaorim@gmail.com>
2025-06-22 22:49:11 -0300

    editei a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
 	return <h1> Essa é minha primeira página na web.
-Te amo, queria que você soubesse!</h1>
+Te amo, queria que você soubesse! Se você me ama, da uma risadinha! Du</h1>
 }
 
 export default Home;
