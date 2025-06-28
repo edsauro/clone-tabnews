df5ad2da7a5c65f0f9024c6e36c05b4f2ca8585b
saorim <edusaorim@gmail.com>
2025-06-28 15:46:41 -0300

    prettier and formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 exercício curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-	return <h1> Essa é minha primeira página na web.
-Te amo, queria que você soubesse! Se você me ama, da uma risadinha! Du</h1>
+  return (
+    <h1>
+      {" "}
+      Essa é minha primeira página na web. Te amo, queria que você soubesse! Se
+      você me ama, da uma risadinha! Du
+    </h1>
+  );
 }
 
 export default Home;
